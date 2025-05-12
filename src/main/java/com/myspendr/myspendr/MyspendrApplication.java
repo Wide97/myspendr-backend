397c95fdf8c7dc4311cc865182ef64a08a77d3a8
Wide97 <mwidesott@gmail.com>
2025-05-12 22:48:25 +0200

    Inizializzazione progetto MySpendr

---
+++ src/main/java/com/myspendr/myspendr/MyspendrApplication.java
@@ -0,0 +1,13 @@
+package com.myspendr.myspendr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyspendrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyspendrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myspendr/myspendr/MyspendrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myspendr.myspendr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyspendrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
